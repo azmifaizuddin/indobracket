de0512793c34ba484f42deb29cad00759a0ee8cc
azmifaizuddin <azmifaizudin@gmail.com>
2020-07-19 16:05:48 +0700

    add function save di question model

--- app/Models/questionModel.php
+++ app/Models/questionModel.php
@@ -5,7 +5,14 @@ use Illuminate\Support\Facades\DB;
 
 class questionModel {
 
-        
+
+
+    public static function save($data){
+        unset($data["_token"]);
+        $new_question = DB::table('questions')->insert($data);
+        return $new_question;
+    }
+
 }
 
 
