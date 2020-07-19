bff5f9feca7fb663b692213fc7b2fe2e2920f636
yusrilramadhan23 <67570255+yusrilramadhan23@users.noreply.github.com>
2020-07-19 16:03:49 +0700

    model get all

--- app/Models/questionModel.php
+++ app/Models/questionModel.php
@@ -4,8 +4,11 @@ namespace App\Models;
 use Illuminate\Support\Facades\DB;
 
 class questionModel {
+    public static get_all(){
+        $questions = DB::table('questions')->get();
+        return $questions;
+    }
 
-        
 }
 
 
