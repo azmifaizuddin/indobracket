210a6d9f8487a7990f75ffd5f60682e705caea2d
yusrilramadhan23 <67570255+yusrilramadhan23@users.noreply.github.com>
2020-07-19 16:10:55 +0700

    ada yang kurang 'function'

--- app/Models/questionModel.php
+++ app/Models/questionModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Support\Facades\DB;
 
 class questionModel {
-    public static get_all(){
+    public static function get_all(){
         $questions = DB::table('questions')->get();
         return $questions;
     }
